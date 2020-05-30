b9a8cb9630397892c94a47afff1719c50fbad70d
Cesar Meloni <cmeloni@patagoniadev.com.ar>
2020-05-30 12:24:14 -0300

    Integracion con VUE

--- src/basic/modules/apiv1/controllers/PermisoController.php
+++ src/basic/modules/apiv1/controllers/PermisoController.php
@@ -7,7 +7,7 @@ use yii\rest\ActiveController;
 /**
  * Default controller for the `apiv1` module
  */
-class UsuarioController extends ActiveController
+class PermisoController extends ActiveController
 {
-    public $modelClass = 'app\modules\apiv1\models\Usuario';
+    public $modelClass = 'app\modules\apiv1\models\Permiso';
 }
---
+++ src/basic/modules/apiv1/models/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\modules\apiv1\models;
+
+class Usuario extends \app\models\Usuario
+{
+    public function fields(){
+        return ['id', 'nombre', 'apellido'];
+    }
+
+    public function extraFields(){
+        return ['edad', 'permisos', 'usuarioPermisos'];
+    }
+}
